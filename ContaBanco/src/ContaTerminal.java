49acadc3ed402a3006d477935bab8b07908947b9
matheusenge <matheusenge@hotmail.com>
2023-08-18 20:10:44 -0300

    Conta Banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, informe seu nome: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da Conta: ");
+        double saldo = scanner.nextDouble();
+
+        String mensagem = String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, " +
+                    "conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, numero, saldo);
+            System.out.println(mensagem);
+
+        scanner.close();
+    }
+}
